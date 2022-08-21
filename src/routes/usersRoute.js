4e9eebe18e05e3bd0a33e1dae95f4feddfad86f2
Gabriel Batalha <gabrielbatalha0902@gmail.com>
2022-08-21 16:49:22 -0300

    Update usersRoute.js

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -2,10 +2,15 @@ const {Router} = require("express");
 const router = Router()
 
 const { usersCreateController } = require("../controllers/usersCreateController");
+const { usersAuthenticateController } = require("../controllers/usersAuthenticateController");
 
 
 
+
+//Users
+router.get("/auth", usersAuthenticateController)
 router.post("/user", usersCreateController);
+
 
 
 module.exports = router
